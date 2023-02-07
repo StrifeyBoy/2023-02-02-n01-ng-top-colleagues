8c7cfc6471e17ebd0251f922a79db8fd1557ce46
Eli N'diath <elinkstrife@gmail.com>
2023-02-07 10:06:20 +0100

    Colleagues Service

--- src/app/shared/components/colleague-list/colleague-list.component.ts
+++ src/app/shared/components/colleague-list/colleague-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {Colleague} from "../../../models/colleague";
 import {LikeHateComponent} from "../like-hate/like-hate.component";
+import {ColleagueService} from "../../../providers/colleague.service";
+
 @Component({
   selector: 'tc-colleague-list',
   templateUrl: './colleague-list.component.html',
@@ -8,38 +10,10 @@ import {LikeHateComponent} from "../like-hate/like-hate.component";
 })
 export class ColleagueListComponent {
 
-  colleaguelist: Colleague[] = [
-    {
-      pseudo: "minimiskine",
-      score:  50,
-      photo: "https://randomuser.me/api/portraits/men/74.jpg",
-    },
-    {
-      pseudo: "megamiskine",
-      score: 100,
-      photo: "https://randomuser.me/api/portraits/men/75.jpg",
-    },
-    {
-      pseudo: 'Galérien',
-      score: 0,
-      photo: 'https://randomuser.me/api/portraits/men/73.jpg',
-    },
-    {
-      pseudo: 'Dolorhombre',
-      score: 12,
-      photo: 'https://randomuser.me/api/portraits/women/75.jpg',
-    },
-    {
-      pseudo: 'Painman',
-      score: 5,
-      photo: 'https://randomuser.me/api/portraits/men/71.jpg',
-    },
-    {
-      pseudo: 'StruggleBoy',
-      score: 5,
-      photo: 'https://randomuser.me/api/portraits/men/79.jpg',
-    },
-  ]
 
+  colleagues: Colleague[] = this.colleagueService.list();
+
+  constructor(private colleagueService: ColleagueService) {
+  }
 
 }
